afe94ee568ef7827f827cfd5a5519cffd86c5b54
Valoth <brock97k@gmail.com>
2017-10-26 00:19:10 -0300

    Revert "Merge branch 'master' of https://github.com/WaterCityFlame/Treble-in-Paradise"
    This reverts commit 0ae7cc9dba19e1dd7cfa63a2beba72faac6e9730, reversing
    changes made to 5d1588efe3d253fd389332b8e8c61a2efb3545ab.

--- src/TrebleInParadise.java
+++ src/TrebleInParadise.java
@@ -17,8 +17,6 @@ import java.io.*;
 import javax.swing.JLayeredPane;
 import java.awt.Font;
 import javax.swing.SwingConstants;
-import java.awt.Color;
-import javax.swing.Timer;
 
 public class TrebleInParadise {
 
@@ -43,7 +41,6 @@ public class TrebleInParadise {
 	private ImagePanel levelLockedBossPanel;
 	private ImagePanel level1Panel;
 	private ImagePanel levelBackgroundPanel;
-	
 
 	/**
 	 * Launch the application.
@@ -85,15 +82,6 @@ public class TrebleInParadise {
         userField.setLocation(482,300);
         loginPanel.add(userField);
         
-        JLabel wrongInfo = new JLabel("Whoops! Looks like you entered the wrong " +
-        "username or password! Please try again.");
-        wrongInfo.setFont(new Font("Serif", Font.PLAIN, 20));
-        wrongInfo.setForeground(Color.red);
-        wrongInfo.setBounds(500, 500, 1000, 50);
-        wrongInfo.setLocation(300,450);
-        wrongInfo.setVisible(false);
-        loginPanel.add(wrongInfo); 
-        
         passField=new JTextField();
         passField.setBounds(5, 5, 280, 50);
         passField.setHorizontalAlignment(JTextField.CENTER);
@@ -108,33 +96,18 @@ public class TrebleInParadise {
         loginPanel.add(startBtn);
         
 		
-      //checks username and password when start button pressed
-      		//and goes to main level screen if successful
-      		startBtn.addActionListener(new ActionListener() {
-      			public void actionPerformed(ActionEvent e) {
-      				String inUser = userField.getText();
-      				String inpass = passField.getText();
-      				if(inUser.equals(username) && inpass.equals(password)) {
-      					loginPanel.setVisible(false);
-      					loginPanel.setEnabled(false); //So action listeners not clickable in other panels
-      					levelScreen();
-      				}
-      				else {
-      					wrongInfo.setVisible(true);
-      				}
-      			}
-      		});
-      		
-      		//removes wrong information message after 10 seconds
-      				int delay = 10000; //milliseconds
-      				   ActionListener taskPerformer = new ActionListener() {
-      				       public void actionPerformed(ActionEvent e) {
-      				           wrongInfo.setVisible(false);
-      				       }
-      				   };
-      				   new Timer(delay, taskPerformer).start();
-      		
-      		
+		//checks username and password when start button pressed
+		//and goes to main level screen if successful
+		startBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String inUser = userField.getText();
+				String inpass = passField.getText();
+				if(inUser.equals(username) && inpass.equals(password)) {
+					loginPanel.setVisible(false);
+					levelScreen();
+				}
+			}
+		});
 		
 	}
 	
@@ -260,10 +233,6 @@ public class TrebleInParadise {
 		
 		ImagePanel level1Panel = new ImagePanel(new ImageIcon("assets"+File.separator+"img"+File.separator+"levelBackgroundGreen.png").getImage());
 		frame.getContentPane().add(level1Panel);
-
-		level1Panel.setVisible(true);
-		Game currentRound = new Game(1);
-		currentRound.display(frame);
 		
 		//Back Button
         JButton backButton = new JButton(new ImageIcon("assets"+File.separator+"img"+File.separator+"backButton.png"));	
@@ -305,102 +274,11 @@ public class TrebleInParadise {
 	     levelText.setLocation(430, 2);
 	     levelText.setOpaque(false);
 	     level1Panel.add(levelText);
-	     
-	     //Treble clef help button
-		    JButton helpButton = new JButton("");
-			helpButton.setBounds(70, 180, 60, 100);
-			helpButton.setOpaque(false);
-			helpButton.setContentAreaFilled(false);
-			helpButton.setBorderPainted(false);
-			level1Panel.add(helpButton);
-			
-			//Treble clef help message
-			JLabel helpMessage = new JLabel("Click on the button that matches the note when" +
-			" it gets to the box.");
-			helpMessage.setFont(new Font("Serif", Font.PLAIN, 35));
-			helpMessage.setBounds(50, 50, 50, 50);
-			helpMessage.setLocation(1050,20);
-			helpMessage.setVisible(false);
-			level1Panel.add(helpMessage);
-			
-			//Treble clef help message
-			JLabel help = new JLabel("Click the treble clef (to the left) for help.");
-			help.setFont(new Font("Serif", Font.PLAIN, 45));
-			help.setBounds(1000, 100, 800, 50);
-			help.setLocation(150,200);
-			help.setVisible(true);
-			level1Panel.add(help);
-			
-			//A Button
-			JButton aButton = new JButton("A");
-			aButton.setBounds(100, 100, 150, 150);
-			aButton.setLocation(25,580);
-			level1Panel.add(aButton); 
-
-			aButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (currentRound.currentNote.equals("a")) {
-						currentRound.points++;	
-						System.out.println("a");
-					}
-				}
-			});
-					
-			//B Button
-			JButton bButton = new JButton("B");
-			bButton.setBounds(100, 100, 150, 150);
-			bButton.setLocation(188,580);
-			level1Panel.add(bButton); 
-					
-			bButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (currentRound.currentNote.equals("b")) {
-						currentRound.points++;	
-						System.out.println("b");
-					}
-				}
-			});
-
-			//C Button
-			JButton cButton = new JButton("C");
-			cButton.setBounds(100, 100, 150, 150);
-			cButton.setLocation(377,580);
-			level1Panel.add(cButton); 
-					
-			//D Button
-			JButton dButton = new JButton("D");
-			dButton.setBounds(100, 100, 150, 150);
-			dButton.setLocation(565,580);
-			level1Panel.add(dButton);
-					
-			//E Button
-			JButton eButton = new JButton("E");
-			eButton.setBounds(100, 100, 150, 150);
-			eButton.setLocation(740,580);
-			level1Panel.add(eButton);
-					
-			//F Button
-			JButton fButton = new JButton("F");
-			fButton.setBounds(100, 100, 150, 150);
-			fButton.setLocation(900,580);
-			level1Panel.add(fButton);
-					
-			//G Button
-			JButton gButton = new JButton("G");
-			gButton.setBounds(100, 100, 150, 150);
-			gButton.setLocation(1060,580);
-			level1Panel.add(gButton);
-			
-			gButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if (currentRound.currentNote.equals("g")) {
-						currentRound.points++;	
-						System.out.println("g");
-					}
-				}
-			});
-
 		
+		level1Panel.setVisible(true);
+		Game currentRound = new Game(1);
+		currentRound.display(frame);
+		currentRound.play();
 		
 		//Label with user's name
 		JLabel userLabel = new JLabel(username);
@@ -417,32 +295,7 @@ public class TrebleInParadise {
 			}
 		});
 		
-		helpButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				help.setVisible(false);
-				helpMessage.setBounds(175, 80, 1000, 297);
-			    helpMessage.setOpaque(false);
-			    level1Panel.add(helpMessage);
-			    helpMessage.setVisible(true);
-				
-			}
-		});
-		
-		//removes help message after 10 seconds
-		int delay = 10000; //milliseconds
-		   ActionListener taskPerformer = new ActionListener() {
-		       public void actionPerformed(ActionEvent e) {
-		           helpMessage.setVisible(false);
-		       }
-		   };
-		   new Timer(delay, taskPerformer).start();
-	
-		
-		currentRound.play();
 	}
-	
-	
 	
 	
 }
